f77d2b07b500fdd8a6f18c09107eeb5ef2458203
Cross <118707038+llCross@users.noreply.github.com>
2023-03-16 01:17:57 +0800

    add prompt and change playerSelection code

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 //write a prompt for the user to input rock paper or scissors
+const playerSelection = prompt('Choose your move!').toLowerCase();
 
-const playerSelection = (userInput) => {
-    if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissor'){console.log(userInput);
-} else {console.log('invalid user input')}
-}
+
 //write a function that generate a random response in the form of rock paper scissors
 const computerSelection = getComputerChoice();
 
@@ -20,7 +18,6 @@ function getComputerChoice () {
 }
 //Write a function that determines the winner and the loser of the game
 function determineOutcome(playerSelection, getComputerChoice) {
-    playerSelection = playerSelection.toLowerCase();
 
     if(playerSelection === computerSelection){
         return 'It\'s a tie!'
@@ -42,6 +39,11 @@ function determineOutcome(playerSelection, getComputerChoice) {
         return 'You Win!'
     }
 }
-
 //write a function that plays a single round of the game
+function playGame () {
+    console.log('You chose:' + playerSelection)
+    console.log('The enemy chose:' + computerSelection)
+    console.log(determineOutcome(playerSelection, computerSelection))
+}
+playGame()
 //Write a function that plays a 5 round game
