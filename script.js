73336b91bd97743db5e1358e74b986c821e6e88e
Cross <118707038+llCross@users.noreply.github.com>
2023-03-17 22:55:30 +0800

    Add scoreboard

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Write a function that keeps tab of scores
+let userScore = parseInt(0);
+let compScore = parseInt(0);
+
 //write a prompt for the user to input rock paper or scissors
 const playerSelection = prompt('Choose your move!').toLowerCase();
 
@@ -16,26 +20,30 @@ function getComputerChoice () {
     }
 }
 //Write a function that determines the winner and the loser of the game
-function determineOutcome(playerSelection, getComputerChoice) {
+function determineOutcome(playerSelection, computerSelection) {
 
     if(playerSelection === computerSelection){
-        return 'It\'s a tie!'
+        return 'It\'s a tie!';
     }
 
     else if(playerSelection === 'rock' && computerSelection === 'paper'){
-        return 'You Lose!'
+        compScore++;
+        return 'You Lose!';
     }
 
     else if(playerSelection === 'paper' && computerSelection === 'scissor'){
-        return 'You Lose!'
+        compScore++;
+        return 'You Lose!';
     }
 
     else if(playerSelection === 'scissor' && computerSelection === 'rock'){
-        return 'You Lose!'
+        compScore++;
+        return 'You Lose!';
     }
     
     else {
-        return 'You Win!'
+        userScore++;
+        return 'You Win!';
     }
 }
 //write a function that plays a single round of the game
@@ -43,6 +51,10 @@ function playGame () {
     console.log('You chose:' + playerSelection)
     console.log('The enemy chose:' + computerSelection)
     console.log(determineOutcome(playerSelection, computerSelection))
+    console.log('User Score: ' + userScore)
+    console.log('Comp Score: ' + compScore)
 }
 playGame()
+
+
 //Write a function that plays a 5 round game
