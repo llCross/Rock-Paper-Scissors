2c7548b5c370d4cb66f7ad9f2778c6686ca4a2a2
Cross <118707038+llCross@users.noreply.github.com>
2023-03-18 21:43:32 +0800

    Put playerSelection and determinceOutcome inside the playRound function and add playGame function to play 5 rounds with 'Game Over' message

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-const getPlayerChoice = playerSelection();
-const getComputerChoice = computerSelection(); 
-
 //Write a function that keeps tab of scores
 let userScore = parseInt(0);
 let compScore = parseInt(0);
 
-//write a prompt for the user to input rock paper or scissors
-
-function playerSelection () {
-    move = prompt('Choose your move!').toLowerCase();
-    return move;
-}
 //write a function that generate a random response in the form of rock paper scissors
 
-function computerSelection () {
+function getComputerChoice () {
     const randomNumber = Math.floor(Math.random()*3);
     switch (randomNumber) {
         case 0:
@@ -26,43 +17,56 @@ function computerSelection () {
 }
 
 
-//Write a function that determines the winner and the loser of the game
-function determineOutcome(getPlayerChoice, getComputerChoice) {   
+//Write a function that plays a single round of the game
+function playRound() {  
 
-    if(getPlayerChoice === getComputerChoice){
-        return 'It\'s a tie!';
+//playerInput
+let playerSelection = prompt("Choose you move!").toLowerCase();
+
+//computerInput
+let computerSelection = getComputerChoice();
+
+//Determines Outcome
+let determineOutcome;
+    if(playerSelection === computerSelection){
+        determineOutcome = 'It\'s a tie!';
     }
 
-    else if(getPlayerChoice === 'rock' && getComputerChoice === 'paper'){
+    else if(playerSelection === 'rock' && computerSelection === 'paper'){
         compScore++;
-        return 'You Lose!';
+        determineOutcome = 'You Lose!';
     }
 
-    else if(getPlayerChoice === 'paper' && getComputerChoice === 'scissor'){
+    else if(playerSelection === 'paper' && computerSelection === 'scissor'){
         compScore++;
-        return 'You Lose!';
+        determineOutcome = 'You Lose!';
     }
 
-    else if(getPlayerChoice === 'scissor' && getComputerChoice === 'rock'){
+    else if(playerSelection === 'scissor' && computerSelection === 'rock'){
         compScore++;
-        return 'You Lose!';
+        determineOutcome = 'You Lose!';
     }
     
     else {
         userScore++;
-        return 'You Win!';
+        determineOutcome = 'You Win!';
     }
+
+//Print Results
+    console.log('You Chose: ' + playerSelection);
+    console.log('Enemy Chose: ' + computerSelection);
+    console.log(determineOutcome);
+    console.log('UserScore: ' + userScore);
+    console.log('Comp Score: ' + compScore);
 }
-//write a function that plays a single round of the game
-function playRound () {
-    console.log('You chose:' + playerSelection ())
-    console.log('The enemy chose:' + computerSelection())
-    console.log(determineOutcome(getPlayerChoice, getComputerChoice))
-    console.log('User Score: ' + userScore)
-    console.log('Comp Score: ' + compScore)
-}
-playRound ();
-//Write a function that plays a 5 round game while also keeping tabs of score
-    for (let i=1;i<5;i++) {
-        playRound ()
+
+//function that play 5 rounds
+function playGame() {
+    for (let i=0;i<5;i++){
+        playRound();
     }
+    console.log('Game Over!');
+    console.log('Final Score: UserScore: ' + userScore + ' Enemy Score: ' + compScore);
+}
+
+playGame();
